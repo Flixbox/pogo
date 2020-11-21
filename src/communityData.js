a1deabf8322872c6d3ef8effbfb2d8ce19b87d62
Felix Tietjen <felix@tietjen.it>
2020-11-22 00:45:37 +0100

    moved communitydata

---
+++ src/communityData.js
@@ -0,0 +1,80 @@
+const areas =  [
+    {
+        title: 'Hamburg (all areas)',
+        communities: [
+            {
+                title: 'pkmngohh',
+                links: [{ title: 'Slack', type: 'slack', href: 'https://bit.ly/38QBidi' }],
+            },
+            {
+                title: 'Pokemon Go Hamburg',
+                links: [{ title: 'Discord', type: 'discord', href: 'https://discord.gg/aQEt6UE' }],
+            },
+            {
+                title: 'PoGo Raiden HH',
+                links: [{ title: 'WhatsApp', type: 'whatsapp', href: 'https://chat.whatsapp.com/FGMI2akTSXE27QsDVjWCJS' }],
+            },
+        ],
+    },
+    {
+        title: 'Hamburg-Nord',
+        communities: [
+            {
+                title: 'HB-Nord',
+                links: [{ title: 'Telegram Talk', type: 'telegram', href: 'https://t.me/joinchat/Flw5mUXyv9TwYTw9q_fT4Q' }],
+            },
+        ],
+    },
+    {
+        title: 'Altona',
+        communities: [
+            {
+                title: 'Altona Raidgruppe',
+                links: [
+                    { title: 'Website', href: 'https://pogoaltona.de/' },
+                    { title: 'Telegram Talk', type: 'telegram', href: 'https://t.me/talkpokemon' },
+                    { title: 'Telegram Raids', type: 'telegram', href: 'https://t.me/joinchat/GfSxL0tmbHqtazBgsTSanQ' },
+                    { title: 'Telegram PvP', type: 'telegram', href: 'https://t.me/joinchat/IZ1tbBPlfzj8pXIKct0m8w' },
+                ],
+            },
+        ],
+    },
+    {
+        title: 'Barmbek',
+        communities: [
+            {
+                title: 'PoGo Barmbek',
+                links: [
+                    { title: 'PoGo Barmbek Raids', type: 'whatsapp', href: 'https://chat.whatsapp.com/JCaUkxVsJz5AdztgD4qk2I' },
+                    { title: 'PoGo Barmbek Talk', type: 'whatsapp', href: 'https://chat.whatsapp.com/GzP8ltIHXBy3lcynr4SY5a' },
+                ],
+            },
+        ],
+    },
+    {
+        title: 'Bergedorf',
+        communities: [
+            {
+                title: 'PoGo Bergedorf',
+                links: [
+                    { title: 'Telegram Smalltalk', type: 'telegram', href: 'https://t.me/joinchat/GQAr8hA7GgQE6vVAOFBtQQ' },
+                    { title: 'Telegram Raids', type: 'telegram', href: 'https://t.me/PoGORaidsBergedorf' },
+                ],
+            },
+            {
+                title: 'Mountain Village Raids',
+                links: [{ title: 'WhatsApp', type: 'whatsapp', href: 'https://chat.whatsapp.com/2MXFOmdPKkfL12y5dfGShz' }],
+            },
+        ],
+    },
+    {
+        title: 'Harburg',
+        communities: [
+            {
+                title: 'Pokemon Go Harburg',
+                links: [{ title: 'Website', href: 'https://www.play-hh.de/' }],
+            },
+        ],
+    },
+]
+export default areas

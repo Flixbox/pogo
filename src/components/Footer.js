6e767939d5555929d903fc3b2a10f3c515945871
Felix Tietjen <felix@tietjen.it>
2020-11-21 15:57:02 +0100

    made footer pretty

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
-import { Box, Link, Typography } from '@material-ui/core'
+import { Box, Link, Typography, useTheme } from '@material-ui/core'
+import github from 'super-tiny-icons/images/svg/github.svg'
 
-const Footer = ({ title, children }) => (
-    <Box mt={1} display="flex" component={Typography}>
-        <Link href="https://github.com/Flixbox/pogo">Repo</Link>
-        Made by Katzdierubbel / Flixbox / Felix
-    </Box>
-)
+const Footer = ({ title, children }) => {
+    const theme = useTheme()
+    return (
+        <Box mt={1} display="flex" justifyContent="space-between" component={Typography}>
+            <Link href="https://github.com/Flixbox/pogo">
+                <img src={github} alt="" style={{ height: '20px', marginRight: theme.spacing(1) }} />
+                Repo
+            </Link>
+            Made by Katzdierubbel / Flixbox / Felix
+        </Box>
+    )
+}
 
 export default Footer

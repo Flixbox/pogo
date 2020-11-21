0746fc06aa90614545f726a7187c1cdae741f360
Felix Tietjen <felix@tietjen.it>
2020-11-21 15:10:37 +0100

    margins

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,10 +1,14 @@
-import { Paper, Typography } from '@material-ui/core'
+import { Box, Paper, Typography } from '@material-ui/core'
 
 const FancyLink = ({ title, children }) => (
-    <Paper p={2}>
-        <Typography variant="h4">{title}</Typography>
-        {children}
-    </Paper>
+    <Box mt={1}>
+        <Paper>
+            <Box p={2}>
+                <Typography variant="h4">{title}</Typography>
+                {children}
+            </Box>
+        </Paper>
+    </Box>
 )
 
 export default FancyLink

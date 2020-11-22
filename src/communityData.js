5bc95c2dd8de059590590e3e2601a6efa221f200
Felix Tietjen <felix@tietjen.it>
2020-11-22 13:50:33 +0100

    added Barmbek list link

--- src/communityData.js
+++ src/communityData.js
@@ -67,7 +67,8 @@ const areas = [
             {
                 title: 'PoGo Barmbek',
                 links: [
-                    { title: 'PoGo Barmbek Raids', type: 'whatsapp', href: 'https://chat.whatsapp.com/JCaUkxVsJz5AdztgD4qk2I' },
+                    { title: 'PoGo Barmbek Raids', type: 'whatsapp', href: 'https://chat.whatsapp.com/EXmQiRXaF9U5nEhXT8IF07' },
+                    { title: 'PoGo Barmbek Raids (old)', type: 'whatsapp', href: 'https://chat.whatsapp.com/JCaUkxVsJz5AdztgD4qk2I' },
                     { title: 'PoGo Barmbek Talk', type: 'whatsapp', href: 'https://chat.whatsapp.com/GzP8ltIHXBy3lcynr4SY5a' },
                 ],
             },
